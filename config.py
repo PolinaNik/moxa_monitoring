2df383340d8b86b6d8d8021fd711a5f673def0f1
PolinaNik <polina-nikitich@yandex.ru>
2021-03-11 11:12:54 +1000

    Добавлен конфиг

---
+++ config.py
@@ -0,0 +1,10 @@
+# Путь к папке с данными программы
+folder_path = '/sintez/sintez/moxa_monitoting/'
+
+# IP адреса MOXA
+ip1 = 'moxa1'
+ip2 = 'moxa2'
+ip3 = 'moxa3'
+ip4 = 'moxa4'
+ip5 = 'moxa1'
+ip6 = 'moxa2'
